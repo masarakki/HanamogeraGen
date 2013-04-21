49249ed96f3e8228a7091c0061db318fd60bac36
Akira Nonaka <nonaka@mac.com>
2013-04-21 10:51:48 +0900

    embed tracking code.

--- src/hana-gethanamogera.rb
+++ src/hana-gethanamogera.rb
@@ -10,5 +10,19 @@ else
   str = ""
 end
 
-header = {'type' => 'text/plain', 'status'=>'OK'}
-cgi.out(header){str}
+header = {'type' => 'text/html', 'status'=>'OK'}
+tracking ="<script type='text/javascript'>
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-5037492-2']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+</script>"
+content_type = "<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>"
+
+page = "<html><header>" + content_type + tracking + "</header><body>" + str + "</body></html>"
+cgi.out(header){page}
