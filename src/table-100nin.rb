c0942834b478fff5dba32a6905eeb9faa05e3e77
Akira Nonaka <nonaka@mac.com>
2013-05-16 17:57:42 +0900

    Added copyright notice.

--- src/table-100nin.rb
+++ src/table-100nin.rb
@@ -1,5 +1,21 @@
 #!/usr/bin/env ruby
 # -*- coding: utf-8 -*-
+
+# Hanamogera Tanka Generator
+# Copyright (C) 2013 Akira Nonaka
+#
+# This file is part of HanamogeraGen.
+#
+#  HanamogeraGen is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or (at
+# your option) any later version.  HanamogeraGen is distributed in the
+# hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+# the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+# PURPOSE. See the GNU General Public License for more details.  You
+# should have received a copy of the GNU General Public License along
+# with HanamogeraGen. If not, see <http://www.gnu.org/licenses/>.
+
 ### 文字(列),発生回数,発生割合,積算発生割合
 $one_char_table = 
 [["あ", 56, 0.112, 0.112],
