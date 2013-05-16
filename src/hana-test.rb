fa6a8c8df5e7544a2926a1e10faf7d7bc001de74
Akira Nonaka <nonaka@mac.com>
2013-05-16 17:53:07 +0900

    Added copyright notice.

--- src/hana-test.rb
+++ src/hana-test.rb
@@ -1,4 +1,20 @@
 #!/usr/bin/env ruby
+
+# Hanamogera Tanka Generator
+# Copyright (C) 2013 Akira Nonaka
+#
+# This file is part of HanamogeraGen.
+#
+#  HanamogeraGen is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or (at
+# your option) any later version.  HanamogeraGen is distributed in the
+# hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+# the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+# PURPOSE. See the GNU General Public License for more details.  You
+# should have received a copy of the GNU General Public License along
+# with HanamogeraGen. If not, see <http://www.gnu.org/licenses/>.
+
 load "hanamogera.rb"
 
 def gen_alot
