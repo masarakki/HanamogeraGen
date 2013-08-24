4ff92d397e7b6488f91aa3026b199b89508f3b1c
masarakki <masaki@hisme.net>
2013-08-24 19:43:40 +0900

    response json

--- src/hana-gethanamogera.rb
+++ src/hana-gethanamogera.rb
@@ -26,19 +26,26 @@ else
   str = ""
 end
 
-header = {'type' => 'text/html', 'status'=>'OK'}
-tracking ="<script type='text/javascript'>
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-5037492-2']);
-  _gaq.push(['_trackPageview']);
 
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-</script>"
-content_type = "<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>"
+if cgi['output'] == 'json'
+  require 'json'
+  header = {'type' => 'application/json', 'status' => 'OK'}
+  page = {:hanamogera => str}.to_json
+else
+  header = {'type' => 'text/html', 'status'=>'OK'}
+  tracking ="<script type='text/javascript'>
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-5037492-2']);
+    _gaq.push(['_trackPageview']);
 
-page = "<html><head>" + content_type + tracking + "</head><body>" + str + "</body></html>"
+    (function() {
+      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+  </script>"
+  content_type = "<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>"
+
+  page = "<html><head>" + content_type + tracking + "</head><body>" + str + "</body></html>"
+end
 cgi.out(header){page}
